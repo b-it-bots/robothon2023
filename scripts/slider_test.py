bc48b201d6f6afae2bc68f5abc81030f9c7ee1a6
RavisankarSelvaraju <ravisankar1223@gmail.com>
2023-04-12 19:57:36 +0200

    [slider-test] add slider front and back motion robot moves to the perceive table pose go to the slider start pose move along the slider length for fixed time and speed move back with same speed

--- scripts/slider_test.py
+++ scripts/slider_test.py
@@ -10,17 +10,39 @@ from geometry_msgs.msg import PoseStamped, Quaternion
 from std_msgs.msg import String
 from robothon2023.full_arm_movement import FullArmMovement
 from robothon2023.slider_action import SliderAction
+from robothon2023.transform_utils import TransformUtils
 import kortex_driver.msg
 import numpy as np
 from kortex_driver.srv import *
 from kortex_driver.msg import *
 import actionlib
 
-class SliderTest(object):
+from utils.force_measure import ForceMeasurmement
 
+class SliderTest(object):
+  
     def __init__(self):
+        print("full arm movement")
         self.fam = FullArmMovement()
-        self.slider_action = SliderAction(self.fam)
+        self.transform_utils = TransformUtils()
+        self.joint_angles = rospy.get_param("~joint_angles", None)
+
+        self.fm = ForceMeasurmement(force_threshold=[10,10,10]) # force threshold in Newtons
+
+        self.setup_arm()
+
+        self.slider_action = SliderAction(self.fam, self.transform_utils)
+
+    def setup_arm(self):
+        """Setup the arm
+        :returns: None
+
+        """
+        # clear faults and subscribe to robot notifications to get feedback from the robot
+        self.fam.clear_faults()
+        self.fam.subscribe_to_a_robot_notification()
+        self.fam.send_joint_angles(self.joint_angles["perceive_table"])
+        self.fam.execute_gripper_command(0.0)
 
     def test(self):
         self.slider_action.do()
--- src/robothon2023/slider_action.py
+++ src/robothon2023/slider_action.py
@@ -1,27 +1,28 @@
-
 #!/usr/bin/env python3
-#
 import tf
 import rospy
 import numpy as np
+import math 
+from kortex_driver.msg import TwistCommand
+
 from robothon2023.abstract_action import AbstractAction
 from robothon2023.full_arm_movement import FullArmMovement
 from geometry_msgs.msg import PoseStamped, Quaternion, Twist, Vector3
 from robothon2023.transform_utils import TransformUtils
-
-from kortex_driver.msg import TwistCommand
+from utils.kinova_pose import KinovaPose, get_kinovapose_from_pose_stamped
+from utils.force_measure import ForceMeasurmement
 
 class SliderAction(AbstractAction):
 
-    def __init__(self, arm: FullArmMovement):
-        super().__init__(arm)
+    def __init__(self, arm: FullArmMovement, transform_utils: TransformUtils):
+        super().__init__(arm, transform_utils)
         self.arm = arm
-    
-        self.tf_utils = TransformUtils()
+        # self.fm = ForceMeasurmement()
+        self.tf_utils = transform_utils
         self.listener = tf.TransformListener()
         self.slider_pose = PoseStamped()
-
-        self.slider_pose_sub = rospy.Subscriber('/task_board_detector/slider_start_pose', PoseStamped, self.slider_pose_callback, queue_size=1)
+        self.slider_pose = rospy.wait_for_message('/task_board_detector/slider_start_pose', PoseStamped)
+        # self.slider_pose_sub = rospy.Subscriber('/mcr_perception/object_selector/output/object_pose', PoseStamped, self.slider_pose_callback, queue_size=10)
         self.cartesian_velocity_pub = rospy.Publisher('/my_gen3/in/cartesian_velocity', TwistCommand, queue_size=1)
         
         print("Slider Action Initialized")
@@ -34,19 +35,43 @@ class SliderAction(AbstractAction):
     def act(self) -> bool:
         print ("in act")
 
+        # velocity_value = 0.01 # m/s
+        distance = 0.045 - 0.005 # m  (0.045 is the length the slider can travel and 0.005 is a small offset for safety reasons)
+        time = 3 # s
+
+        velocity_value = round(distance/time , 3) # m/s
+
+        self.slider_velocity_twist = self.create_twist_from_velocity(velocity_value)
+
+        rospy.loginfo(">> Moving arm to slider <<")
         self.move_arm_to_slider()
 
-        velocity_value = 0.05
-        self.slider_velocity_twist = self.create_twist_from_velocity(velocity_value)
-        
+        rospy.loginfo(">> Clossing gripper <<")
+        self.arm.execute_gripper_command(0.85) # close gripper to 85% 
+
+        rospy.loginfo(">> Moving arm along the slider <<")
         self.move_arm_along_slider()
-        rospy.sleep(3.0)
+        rospy.sleep(time)
+
+        rospy.loginfo(">> Stopping arm <<")
         self.stop_arm()
+        rospy.sleep(1)
+
+        rospy.loginfo(">> Moving slider back  <<")
+        self.move_slider_back()
+        rospy.sleep(time)
+
+        rospy.loginfo(">> Stopping arm <<")
+        self.stop_arm()
+
+        rospy.loginfo(">> Process finished successfully <<")
 
         return True
 
     def verify(self) -> bool:
         print ("in verify")
+
+        ## can be verified by checking the current location of the EEF 
         return True
 
     def do(self) -> bool:
@@ -57,13 +82,9 @@ class SliderAction(AbstractAction):
         """
         Callback function for slider pose
         """
-        self.slider_pose.pose.position.x = msg.pose.position.x
-        self.slider_pose.pose.position.y = msg.pose.position.y
-        self.slider_pose.pose.position.z = msg.pose.position.z
-        self.slider_pose.pose.orientation.x = msg.pose.orientation.x
-        self.slider_pose.pose.orientation.y = msg.pose.orientation.y
-        self.slider_pose.pose.orientation.z = msg.pose.orientation.z
-        self.slider_pose.pose.orientation.w = msg.pose.orientation.w
+        print("Slider pose received")
+
+        self.slider_pose = msg.pose
 
 
     def create_twist_from_velocity(self, velocity) -> Twist:
@@ -87,28 +108,53 @@ class SliderAction(AbstractAction):
         velocity_cmd.twist.linear_z = velocity.linear.z
 
         return velocity_cmd
+    
 
+    def rotate_Z_down(self, msg: PoseStamped) -> PoseStamped:
+        
+        msg = msg
+        msg.header.stamp = rospy.Time.now()
+        q = list(tf.transformations.quaternion_from_euler(math.pi, 0.0, math.pi/2))
+        msg.pose.orientation = Quaternion(*q)
+        msg.pose.position.z += 0.01
+        msg = self.transform_utils.transformed_pose_with_retries(msg, 'base_link')
+        return msg
 
     def move_arm_to_slider(self):
         """
         Move arm to along slider in with velocity vector
         """
 
-        rospy.loginfo(" ===== >> Moving arm to slider << ======")
+        rospy.loginfo("slider pose below")
         print(self.slider_pose)
-        self.arm.send_cartesian_pose(self.slider_pose)
-        rospy.sleep(1.0)
-        rospy.loginfo(" ===== >> stop << ======")
+        slider_pose = self.rotate_Z_down(self.slider_pose)
+        pose_to_send = get_kinovapose_from_pose_stamped(slider_pose)
+        self.arm.send_cartesian_pose(pose_to_send)
+        return True
 
 
     def move_arm_along_slider(self):
         """
         Move arm along slider in with velocity vector
         """
+
+        print("twist value below")
         print(self.slider_velocity_twist)
         slider_velocity = self.convert_twist_twistcommand(self.slider_velocity_twist)
         self.cartesian_velocity_pub.publish(slider_velocity)
+        return True
 
+    def move_slider_back(self):
+        """
+        Move arm back
+        """
+
+        print("twist value below")
+        print(self.slider_velocity_twist)
+        self.slider_velocity_twist.linear.y = -self.slider_velocity_twist.linear.y
+        slider_velocity = self.convert_twist_twistcommand(self.slider_velocity_twist)
+        self.cartesian_velocity_pub.publish(slider_velocity)
+        return True
 
     def stop_arm(self):
         """
@@ -154,22 +200,3 @@ class SliderAction(AbstractAction):
 
 
 
-
-
-## CODE FLOW ##
-
-    # 1. SliderAction is initialized with the arm object
-
-    # 2. SliderAction subscribes to slider_pose topic
-
-    # 3. SliderAction creates slider_direction vector
-
-    # 4. SliderAction converts slider_direction vector to base frame
-
-    # 5. SliderAction moves arm to slider
-
-    # 6. SliderAction sends slider_direction vector to arm
-
-    # 7. SliderAction moves arm along slider
-
-    # 8. SliderAction stops arm
